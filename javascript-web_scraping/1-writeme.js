3254afd25b77644ddaede0fff798b82f3dc4e621
victorfabian22 <fabianromeroa22@gmail.com>
2023-10-27 00:33:34 +0000

    update

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-// fs = module for working with files.
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
+const argv = process.argv;
+
+fs.writeFile(argv[2], argv[3], function (error) {
+  if (error) {
+    console.log(error);
   }
 });
