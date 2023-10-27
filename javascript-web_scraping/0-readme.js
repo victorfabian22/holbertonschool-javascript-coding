dbad325ff970a05bee3a8e19ee46f497aa158890
victorfabian22 <fabianromeroa22@gmail.com>
2023-10-27 00:25:40 +0000

    update

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-const reader = fs.createReadStream(file, 'utf-8');
 
-reader.on('error', (error) => {
-  console.error(error);
-});
-
-reader.on('data', (data) => {
-  console.log(data);
-});
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+})
+;
