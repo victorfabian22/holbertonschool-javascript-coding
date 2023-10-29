ec71351abfee23ce2387d9dd0b596f61f83f7be7
victorfabian22 <fabianromeroa22@gmail.com>
2023-10-29 22:54:01 +0000

    update

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-/* task 2 statuscode js */
 
 const request = require('request');
-request(process.argv[2], function (error, response) {
-  if (error == null) {
-    console.log('code: ' + response.statusCode);
-  }
+const url = process.argv[2];
+request.get(url).on('response', function (response) {
+  console.log('code: ' + response.statusCode);
 });
