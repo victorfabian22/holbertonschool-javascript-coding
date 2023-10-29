6d556abda3258c149eb4212235175621f752a5f0
victorfabian22 <fabianromeroa22@gmail.com>
2023-10-29 22:56:45 +0000

    update

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
-const MovieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + MovieId;
-request.get(url, function (err, response, body) {
+const url = process.argv[2];
+request.get(url, (err, respose, body) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(JSON.parse(body).title);
+    const films = JSON.parse(body).results;
+    let ct = 0;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.includes('18')) {
+          ct++;
+        }
+      }
+    }
+    console.log(ct);
   }
 });
