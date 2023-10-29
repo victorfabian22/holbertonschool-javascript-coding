38defa4e78df631e7746432ac9fdc17dd3439f7e
victorfabian22 <fabianromeroa22@gmail.com>
2023-10-29 22:41:16 +0000

    update

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
-/* task 0 readme js */
-
 const fs = require('fs');
 
 if (process.argv.length !== 3) {
-  console.error('Usage: node 0-readme.js <file-path>');
+  console.error('Uso: node 0-readme.js <ruta del archivo>');
   process.exit(1);
 }
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
   } else {
     console.log(data);
   }
