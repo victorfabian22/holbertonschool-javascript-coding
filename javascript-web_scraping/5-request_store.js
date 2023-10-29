04bb2114826a79ae5b3fc8f3b359280d7f590335
victorfabian22 <fabianromeroa22@gmail.com>
2023-10-29 23:05:05 +0000

    update

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,19 +1,9 @@
 #!/usr/bin/node
+
 const request = require('request');
 const fs = require('fs');
 
-const URL = process.argv[2];
+const url = process.argv[2];
+const FileName = process.argv[3];
 
-const file = process.argv[3];
-
-request.get(URL, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  }
-
-  fs.writeFile(file, body, { encoding: 'utf-8' }, (err) => {
-    if (err) {
-      console.error('Error:', err);
-    }
-  });
-});
+request.get(url).pipe(fs.createWriteStream(FileName));
